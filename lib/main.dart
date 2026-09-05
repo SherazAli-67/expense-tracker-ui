822c83a67e9a9a7ce803432a093eb5edabd1c360
sheraz-ali <soomrosheraz054@gmail.com>
2026-09-05 23:03:13 +0500

    Expense Tracker UI - Development
    1. Project structure set

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'constants/string_const.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Expense Tracker',
+      title: StringConst.appTitle,
       theme: ThemeData(
-
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
       home: Scaffold(body: Center(child: Text("Expense Tracker app UI"),),)
